a2f61331cb942390530d2b3c549d75be24f09ddd
aburgess32 <getfreqqi@gmail.com>
2026-04-01 08:53:46 +0000

    Update navbar.php brand to match navhome.php: logo + Community Learning Platform
    navbar.php is used by all non-homepage pages (browse, directory, tutorials, etc.)

--- htdocs/navbar.php
+++ htdocs/navbar.php
@@ -34,7 +34,7 @@
 	<!-- Navbar-->
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
 		<!-----user name code here-->
-    <a href="" class="navbar-brand" href="index.php"><i class="fa fa-fw fa-user"></i> EDUCATION WITH MODERN TECHNOLOGY</a>
+    <a class="navbar-brand" href="index.php"><img src="assets/img/edutek-logo.jpg" alt="EduTek" class="navbar-brand-logo"> Community Learning Platform</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>

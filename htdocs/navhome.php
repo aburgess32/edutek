f433f5d0fe39efccc119385492285650c93f38e1
aburgess32 <getfreqqi@gmail.com>
2026-04-01 08:52:09 +0000

    Increase hero height to 540px, rename navbar brand to Community Learning Platform

--- htdocs/navhome.php
+++ htdocs/navhome.php
@@ -46,7 +46,7 @@ function myFunction() {
 	<!-- Navbar-->
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
 		<!-----user name code here-->
-    <a class="navbar-brand" href="index.php"><img src="assets/img/edutek-logo.jpg" alt="EduTek" class="navbar-brand-logo"> EduTek</a>
+    <a class="navbar-brand" href="index.php"><img src="assets/img/edutek-logo.jpg" alt="EduTek" class="navbar-brand-logo"> Community Learning Platform</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>

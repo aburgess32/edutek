8caa12291a0e35fd801cd0640c3f9d5fbef8f8db
Freqqi <thecuriousityspark@gmail.com>
2026-04-08 00:01:12 +0000

    FRE-54: Fix hardcoded /logout.php and /find-user.php paths in avatar-bubble.js

--- htdocs/js/avatar-bubble.js
+++ htdocs/js/avatar-bubble.js
@@ -63,7 +63,7 @@
         '</div>' +
       '</div>' +
       '<div class="mini-dash-footer">' +
-        '<a href="/logout.php" class="mini-dash-switch" style="text-decoration:none;display:block;text-align:center;">Switch User</a>' +
+        '<a href="' + (window.EDUTEK_BASE || '') + '/logout.php" class="mini-dash-switch" style="text-decoration:none;display:block;text-align:center;">Switch User</a>' +
       '</div>';
     document.body.appendChild(dash);
 
@@ -139,11 +139,11 @@
           showInlineConfirm(wrap, data);
         } else {
           // Not found — go to find-user page
-          window.location.href = '/find-user.php';
+          window.location.href = (window.EDUTEK_BASE || '') + '/find-user.php';
         }
       })
       .catch(function() {
-        window.location.href = '/find-user.php';
+        window.location.href = (window.EDUTEK_BASE || '') + '/find-user.php';
       })
       .finally(function() {
         goBtn.textContent = 'Go';
